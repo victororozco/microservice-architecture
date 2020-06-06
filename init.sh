ec14059bbbe4b8b35204a676ef7747305f9f1be6
Victor Orozco <vorozco@vidaplatform.com>
2020-06-06 19:25:44 -0400

    [IMP] Moved 'leads' and 'auth' to the "modules" folder.

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@ title="VidaPlatform"
 echo "$title"
 
 command="docker-compose -f docker-compose.yml \
-                  -f leads/docker-compose.yml \
-                  -f auth/docker-compose.yml \
+                  -f modules/leads/docker-compose.yml \
+                  -f modules/auth/docker-compose.yml \
                   $1"
 
 $command;
