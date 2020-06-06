56ef000ef428fde19c5772440fc8c29b04887c35
Victor Orozco <vorozco@vidaplatform.com>
2020-06-06 16:37:47 -0400

    [IMP] Improvement in init.sh script and update README.

--- README.md
+++ README.md
@@ -33,3 +33,25 @@ PGDATA=/var/lib/postgresql/data/pgdata
 POSTGRES_PORT=5432
 POSTGRES_HOST=postgres-db
 ```
+
+Commands `init.sh`:
+
+- Build images:
+```sh
+sh init.sh build
+```
+
+- Run the project:
+```sh
+sh init.sh up
+```
+
+- Down the project:
+```sh
+sh init.sh down
+```
+
+- Others commands:
+```sh
+sh init.sh "command to exec"
+```
--- init.sh
+++ init.sh
@@ -1,37 +1,10 @@
-title="Vidaplatform menú"
-prompt="Pick an option:"
-options=(
-  "Build" \
-  "Up" \
-  "Down"
-)
+title="VidaPlatform"
 
 echo "$title"
-PS3="$prompt "
 
-build="docker-compose -f docker-compose.yml \
+command="docker-compose -f docker-compose.yml \
                   -f leads/docker-compose.yml \
-                  build"
+                  -f auth/docker-compose.yml \
+                  $1"
 
-up="docker-compose -f docker-compose.yml \
-                  -f leads/docker-compose.yml \
-                  up"
-
-down="docker-compose -f docker-compose.yml \
-                  -f leads/docker-compose.yml \
-                  down"
-
-
-select opt in "${options[@]}" "Quit"; do 
-
-    case "$REPLY" in
-      1 ) $UP; break;;
-      2 ) $up; break;;
-      3 ) $down; break;;
-
-      $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
-      *) echo "Invalid option. Try another one.";continue;;
-
-    esac
-
-done
+$command;
